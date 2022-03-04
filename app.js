4640479bcfa6414bfefa83d98c70710a344df8c7
marianonu14 <marianonunezma@gmail.com>
2022-03-03 22:09:31 -0300

    First Functions

--- app.js
+++ app.js
@@ -1,9 +1,68 @@
-const Elements = ['Rock', 'Paper', 'Scissor'];
 
-function computerPlay() {
-    var random = Math.floor(Math.random()*Elements.length);
-    return Elements[random];
+
+let computerSelection = randomComputer();
+let playerSelection = userSelection();
+let computerScore = 0;
+let playerScore = 0;
+
+
+function randomComputer(){
+    const options = ['rock', 'paper', 'scissor'];
+    return (options[Math.floor(Math.random() *options.length)])
 }
 
-console.log(computerPlay());
+function userSelection(){
+    return prompt("Choose Rock, Paper or Scissor") 
+}
+
+function validateString(){
+    let newString = playerSelection.toLowerCase();
+
+    const options = ['rock', 'paper', 'scissor'];
+    
+    if(!options.includes(newString)){
+        return alert('Recarga la Pagina');
+    }
+}
+
+
+function playRound(playerSelection,computerSelection){
+    
+    validateString();
+
+      if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+        console.log('Empate');
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        console.log('Ganaste');
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor') {
+        console.log('Ganaste');
+    }
+    
+      if (playerSelection.toLowerCase() ==='paper' && computerSelection === 'paper'){
+        console.log('Empate');
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissor') {
+        console.log('Perdiste');
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        console.log('Ganaste');
+    }
+    
+      if (playerSelection.toLowerCase() ==='scissor' && computerSelection === 'scissor'){
+        console.log('Empate');
+    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'rock') {
+        console.log('Perdiste');
+    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'paper') {
+        console.log('Ganaste');
+    }
+}
+
+playRound(playerSelection,computerSelection); 
+
+
+
+
+
+
+
+
+    
 
