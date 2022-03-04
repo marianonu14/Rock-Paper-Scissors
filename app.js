2325c2485684a9140b094d56b1bac7052aa20cfe
marianonu14 <marianonunezma@gmail.com>
2022-03-04 19:12:25 -0300

    Last Commit

--- app.js
+++ app.js
@@ -1,61 +1,52 @@
+let computerSelection;
+let playerSelection;
 
 
-let computerSelection = randomComputer();
-let playerSelection = userSelection();
-let computerScore = 0;
-let playerScore = 0;
+const myArray = ["Rock", "Paper", "Scissors"];
 
-
-function randomComputer(){
-    const options = ['rock', 'paper', 'scissor'];
-    return (options[Math.floor(Math.random() *options.length)])
-}
-
-function userSelection(){
-    return prompt("Choose Rock, Paper or Scissor") 
-}
-
-function validateString(){
-    let newString = playerSelection.toLowerCase();
-
-    const options = ['rock', 'paper', 'scissor'];
-    
-    if(!options.includes(newString)){
-        return alert('Recarga la Pagina');
-    }
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
 }
 
 
 function playRound(playerSelection,computerSelection){
     
-    validateString();
-
-      if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
-        console.log('Empate');
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        console.log('Ganaste');
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor') {
-        console.log('Ganaste');
-    }
-    
-      if (playerSelection.toLowerCase() ==='paper' && computerSelection === 'paper'){
-        console.log('Empate');
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissor') {
-        console.log('Perdiste');
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        console.log('Ganaste');
-    }
-    
-      if (playerSelection.toLowerCase() ==='scissor' && computerSelection === 'scissor'){
-        console.log('Empate');
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'rock') {
-        console.log('Perdiste');
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'paper') {
-        console.log('Ganaste');
-    }
+    playerSelection = window.prompt("Choose Rock, Paper or Scissor").toLocaleLowerCase();
+   
+    computerSelection = computerPlay().toLocaleLowerCase();
+   
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+      console.log('Empate');
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+      console.log('Ganaste');
+  } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+      console.log('Ganaste');
+  }
+  
+    if (playerSelection ==='paper' && computerSelection === 'paper'){
+      console.log('Empate');
+  } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+      console.log('Perdiste');
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+      console.log('Ganaste');
+  }
+  
+    if (playerSelection ==='scissor' && computerSelection === 'scissor'){
+      console.log('Empate');
+  } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+      console.log('Perdiste');
+  } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+      console.log('Ganaste');
+  }   
 }
 
-playRound(playerSelection,computerSelection); 
+for (let i = 0; i < 5; i++) {
+    playRound(playerSelection,computerSelection);
+}
+
+
+
+
 
 
 
