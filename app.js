20e36408a9a382a0e76c89c87defbb7e3bd89930
marianonu14 <marianonunezma@gmail.com>
2022-03-06 16:58:20 -0300

    Last Commit

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 let playerCount = 0;
 let computerCount = 0;
 
+const playerSelectionButton = document.querySelectorAll('button');
 const playerValue = document.querySelector('#player-score')
 const computerValue = document.querySelector('#computer-score')
-const playerSelectionButton = document.querySelectorAll('button');
 const winnerContainer = document.querySelector('#winner')
 
 winnerContainer.textContent = ''
 
 playerSelectionButton.forEach(function(playerSelection){
      playerSelection.addEventListener('click', (e) => {
-     let selection = e.target.value
-     playRound(selection);
-    }) 
-});
+        let selection = e.target.value
+        playRound(selection); 
+       })  
+     });
+ 
 
 function computerPlay() {
     const myArray = ['rock', 'paper', 'scissor'];
@@ -21,11 +22,17 @@ function computerPlay() {
   }
 
 function playRound(selection){
+  computerSelection = computerPlay();
 
-    const computerSelection = computerPlay();
+  checkCount(selection);
+  
+  computerValue.textContent = computerCount;
+  playerValue.textContent = playerCount;
+};
 
-   if (selection === 'rock' && computerSelection === 'paper') {
-        playerCount+=1
+function startCounting(selection) {
+    if (selection === 'rock' && computerSelection === 'paper') {
+        computerCount+=1
   } else if (selection === 'rock' && computerSelection === 'scissor') {
         playerCount+=1;
   }
@@ -41,35 +48,29 @@ function playRound(selection){
   } else if (selection === 'scissor' && computerSelection === 'paper') {
       playerCount+=1;
   }   
-
-  computerValue.textContent = computerCount;
-  
-  playerValue.textContent = playerCount;
-
-  checkCount();
 };
 
-function checkCount(){
+function checkCount(selection){
   if (playerCount === 5) {
         winnerContainer.textContent = 'You Win'
-        setTimeout(resetCount, 500);
+        setTimeout(resetCount, 800);
   } else if (computerCount === 5) {
         winnerContainer.textContent = 'Computer Win'
-        setTimeout(resetCount, 500);
-    };
-}
+        setTimeout(resetCount, 800);
+  } else {
+        startCounting(selection); 
+    }
+};
 
 function resetCount (){
+    playerSelectionButton
     computerValue.textContent = 0;
     playerValue.textContent  = 0;
     playerCount = 0;
     computerCount = 0;
     setTimeout(resetContainer, 1500);
-}
+};
 
 function resetContainer(){
     winnerContainer.textContent = ""
-}
-
-
-
+};
